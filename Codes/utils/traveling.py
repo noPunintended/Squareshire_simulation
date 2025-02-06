304f6123363c2537e295c005a69a979bc9e23435
Punny <pannathorn.naksung@gmail.com>
2025-02-06 17:36:33 +0000

    [ADD]  Return current position

--- Codes/utils/traveling.py
+++ Codes/utils/traveling.py
@@ -19,20 +19,38 @@ def generate_random_value(rates, size=1):
     else: return np.null 
 
 
-def calculate_distance(origin, destination):
+def calculate_distance(origin_x, origin_y, destination_x, destination_y):
 
-    return np.sqrt((origin[0] - destination[0])**2 + (origin[1] - destination[1])**2)
+    return np.sqrt((origin_x - destination_x)**2 + (origin_y - destination_y)**2)
 
 
-def calculate_travel(origin, destination):
+def calculate_travel(origin_x, origin_y, destination_x, destination_y):
 
-    distances = calculate_distance(origin, destination)
+    distances = calculate_distance(origin_x, origin_y, destination_x, destination_y)
     print(distances)
     expected_travel_time = distances/ rates['trip']['avg_travel_speed']
     print(expected_travel_time)
-    actual_travel_time = expected_travel_time * generate_random_value(rates['trip']['actual_trip_bound'])
+    travel_rates = generate_random_value(rates['trip']['actual_trip_bound'])
+    actual_travel_time = expected_travel_time * travel_rates
 
-    return expected_travel_time, actual_travel_time
+    return expected_travel_time, actual_travel_time, travel_rates
 
+
+def return_current_pos(origin_x, origin_y, destination_x, destination_y, travel_rates, travel_time, departure_time, current_time):
+
+    current_trip_time = current_time - departure_time
+    dist_x = (destination_x - origin_x)
+    dist_y = (destination_y - origin_y)
+    avg_speed_x = dist_x / travel_time
+    avg_speed_y = dist_y / travel_time
+    dist_x = current_trip_time * avg_speed_x
+    dist_y = current_trip_time * avg_speed_y
+    current_x = origin_x + dist_x
+    current_y = origin_y + dist_y
+    return current_x, current_y
+
+## To save in calculation origin, destination, travel_rates, depart_time, status
 rates = read_rates_config('configs.yaml')
-print(calculate_travel((1,1), (10, 13)))
+print(calculate_travel(1, 1, 10, 13))
+print(return_current_pos(np.array([1, 2]), np.array([1, 8]), np.array([10, -9]), np.array([13, 6]), 
+                            np.array([1.0, 0.9]), np.array([45.0, 100]), np.array([0, 0]), 10))
